691dd34282785adb90aae01032eef95080109717
Alex Sigalos <alex.wayne.dev@gmail.com>
2025-07-12 04:31:35 -0700

    core functionality working

---
+++ core.lua
@@ -0,0 +1,56 @@
+local M = {}
+
+local naughty = require("naughty")
+local awful = require("awful")
+local state = require("droplet.state")
+local widget = require("droplet.widget")
+
+function M:start(interval_ms)
+	state.running = true
+	if state.process then
+		if state.process then
+			local pid = tonumber(state.process)
+			if pid then
+				awful.spawn.easy_async_with_shell("kill " .. pid, function()
+					state.process = nil
+				end)
+			end
+		end
+	end
+
+	local cmd = string.format("python3 .config/awesome/droplet/droplet.py %d", interval_ms or 300)
+	state.process = awful.spawn.with_line_callback(cmd, {
+		stdout = function(line)
+			state.active_color, state.contrast_color = line:match("^(#%x%x%x%x%x%x)%s+(#%x%x%x%x%x%x)")
+			if state.active_color and state.contrast_color then
+				widget.update()
+			end
+		end,
+		stderr = function(line)
+			naughty.notify({
+				text = line,
+				timeout = 5,
+				position = "top_right",
+				screen = awful.screen.focused(),
+			})
+		end,
+		exit = function()
+			state.running = false
+			state.process = nil
+		end,
+	})
+end
+
+function M:stop()
+	if state.process then
+		local pid = tonumber(state.process)
+		if pid then
+			awful.spawn.easy_async_with_shell("kill " .. pid, function()
+				state.process = nil
+				state.running = false
+			end)
+		end
+	end
+end
+
+return M
---
+++ state.lua
@@ -0,0 +1,8 @@
+local M = {
+	running = false,
+	process = nil,
+	active_color = nil,
+	contrast_color = nil,
+}
+
+return M
